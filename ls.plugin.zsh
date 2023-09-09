9cee80d821436c4a46a6e99b4dbdda4a86831ca3
Samuel Wu <twopizza9621536@gmail.com>
2023-09-09 11:58:11 -0400

    fix: conflicted aliases and quotes
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

--- ls.plugin.zsh
+++ ls.plugin.zsh
@@ -3,8 +3,8 @@
 export TIME_STYLE="${TIME_STYLE:-long-iso}"
 
 alias ls='ls -Fgh --group-directories-first --color=auto'
-alias l.='ls -d .*'
-alias lS='ls -1'
+alias lh='ls -d .*'
+alias lc='ls -1'
 
 alias ll='ls -l'
 alias la='ll -A'
@@ -13,7 +13,7 @@ alias lA='ll -tu'
 alias lC='ll --time=birth'
 alias lM='ll -t'
 alias lS='ll -S'
-alias lX="ll -X"
+alias lX='ll -X'
 alias llm='lM'
 
 alias l='ll -a'
